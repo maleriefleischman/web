61698a24f9f97503e969b312656fc257a3b3acde
Malerie Fleischman <mal.fleischman@gmail.com>
2019-02-09 02:41:00 +0000

    changed title, added boostrap

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//bootstrap
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules','bootstrap','dist')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
