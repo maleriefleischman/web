b445b7e413aeae24587213c2d53fcf7aa388cde6
Malerie Fleischman <mal.fleischman@gmail.com>
2019-02-09 22:16:13 +0000

    made jquery publically available and listed in hmtl head

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //bootstrap
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules','bootstrap','dist')));
-
+app.use('/jquery', express.static(path.join(__dirname, 'node_modules','jquery','dist')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
