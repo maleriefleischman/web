a41646f630dd78f0a5c39b2a9d8093d5378f6e47
Malerie Fleischman <mal.fleischman@gmail.com>
2019-02-13 06:38:46 +0000

    added route /timer for react app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Malerie Fleischman' });
+});
+
+router.get('/timer', function(req, res, next){
+  res.render('timer', { title: 'React Timer'});
 });
 
 module.exports = router;
